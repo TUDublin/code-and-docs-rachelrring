1ce45869b3d2fffc43ac1cfbce935b50939d532d
Rachel Ring <rachelring84@gmail.com>
2023-11-28 14:39:41 +0000

    Fixed routing

--- client-side/budget-app/src/app/app.routes.ts
+++ client-side/budget-app/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BudgetPlannerComponent } from './budget-planner/budget-planner.component';
+import { RouterLink } from '@angular/router';
 
 export const routes: Routes = [
     {path: 'budget-planner', component: BudgetPlannerComponent },

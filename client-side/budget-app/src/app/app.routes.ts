f94b47e80a52fc2a264e6921b637d628bd5993b9
Rachel Ring <rachelring84@gmail.com>
2023-11-28 14:27:49 +0000

    Added initial navigation componant

--- client-side/budget-app/src/app/app.routes.ts
+++ client-side/budget-app/src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BudgetPlannerComponent } from './budget-planner/budget-planner.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'budget-planner', component: BudgetPlannerComponent },
+];
---
+++ client-side/budget-app/src/app/budget-planner/budget-planner.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-budget-planner',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './budget-planner.component.html',
+  styleUrl: './budget-planner.component.css'
+})
+export class BudgetPlannerComponent {
+
+}

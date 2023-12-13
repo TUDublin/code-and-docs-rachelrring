39e5dca92b4bc603d651d575d90c45c3fa4674a8
Rachel Ring <rachelring84@gmail.com>
2023-12-13 12:00:22 +0000

    Started form for budget planning - I've added one input box for income

--- client-side/budget-app/src/app/budget-planner/budget-planner.component.ts
+++ client-side/budget-app/src/app/budget-planner/budget-planner.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-budget-planner',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
   templateUrl: './budget-planner.component.html',
   styleUrl: './budget-planner.component.css'
 })
 export class BudgetPlannerComponent {
-
+  income = new FormControl('');
 }

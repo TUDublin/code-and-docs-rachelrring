c4d820af1131e650364d08e6872baafeb3e85de6
Rachel Ring <rachelring84@gmail.com>
2024-01-18 16:04:07 +0000

    function getChartData() sums each section for the doughnut chart

--- client-side/budget-app/src/app/budget-planner/budget-planner.component.ts
+++ client-side/budget-app/src/app/budget-planner/budget-planner.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Chart } from 'chart.js/auto';
 
 @Component({
@@ -26,64 +26,66 @@ export class BudgetPlannerComponent {
 
   constructor(private fb: FormBuilder) {
     this.myForm = this.fb.group({
-      incomePay: ['', Validators.required],
-      incomeBenefits: ['', Validators.required],
-      incomePension: ['', Validators.required],
-      incomeOther: ['', Validators.required],
+      incomePay: [0.00, Validators.required],
+      incomeBenefits: [0.00, Validators.required],
+      incomePension: [0.00, Validators.required],
+      incomeOther: [0.00, Validators.required],
 
-      paymentMortgage: ['', Validators.required],
-      paymentRent: ['', Validators.required],
-      paymentHomeInsurance: ['', Validators.required],
+      paymentMortgage: [0.00, Validators.required],
+      paymentRent: [0.00, Validators.required],
+      paymentHomeInsurance: [0.00, Validators.required],
 
-      paymentHouseTax: ['', Validators.required],
-      paymentHouseGas: ['', Validators.required],
-      paymentElectricity: ['', Validators.required],
-      paymentWater: ['', Validators.required],
-      paymentHomePhone: ['', Validators.required],
-      paymentMobilePhone: ['', Validators.required],
-      paymentBroadband: ['', Validators.required],
-      paymentTvLicense: ['', Validators.required],
-      paymentHomeMaintenance: ['', Validators.required],
+      paymentHouseTax: [0.00, Validators.required],
+      paymentHouseGas: [0.00, Validators.required],
+      paymentElectricity: [0.00, Validators.required],
+      paymentWater: [0.00, Validators.required],
+      paymentHomePhone: [0.00, Validators.required],
+      paymentMobilePhone: [0.00, Validators.required],
+      paymentBroadband: [0.00, Validators.required],
+      paymentTvLicense: [0.00, Validators.required],
+      paymentHomeMaintenance: [0.00, Validators.required],
 
-      paymentGroceries: ['', Validators.required],
-      paymentTakeaways: ['', Validators.required],
-      paymentCigarettes: ['', Validators.required],
-      paymentEatingOut: ['', Validators.required],
-      paymentClothing: ['', Validators.required],
-      paymentChildcare: ['', Validators.required],
-      paymentHealthandBeauty: ['', Validators.required],
-      paymentEyeCare: ['', Validators.required],
-      paymentDentalCare: ['', Validators.required],
-      paymentMedicine: ['', Validators.required],
+      paymentGroceries: [0.00, Validators.required],
+      paymentTakeaways: [0.00, Validators.required],
+      paymentCigarettes: [0.00, Validators.required],
+      paymentEatingOut: [0.00, Validators.required],
+      paymentClothing: [0.00, Validators.required],
+      paymentChildcare: [0.00, Validators.required],
+      paymentHealthandBeauty: [0.00, Validators.required],
+      paymentEyeCare: [0.00, Validators.required],
+      paymentDentalCare: [0.00, Validators.required],
+      paymentMedicine: [0.00, Validators.required],
 
-      paymentActivities: ['', Validators.required],
-      paymentPocketMoney: ['', Validators.required],
-      paymentChildSupport: ['', Validators.required],
-      paymentSchoolFees: ['', Validators.required],
+      paymentActivities: [0.00, Validators.required],
+      paymentPocketMoney: [0.00, Validators.required],
+      paymentChildSupport: [0.00, Validators.required],
+      paymentSchoolFees: [0.00, Validators.required],
 
-      paymentPetFood: ['', Validators.required],
-      paymentVetBills: ['', Validators.required],
+      paymentPetFood: [0.00, Validators.required],
+      paymentVetBills: [0.00, Validators.required],
 
-      paymentLifeInsurance: ['', Validators.required],
-      paymentHealthInsurance: ['', Validators.required],
-      paymentDentalInsurance: ['', Validators.required],
-      paymentPetInsurance: ['', Validators.required],
-      paymentCarInsurance: ['', Validators.required],
+      paymentLifeInsurance: [0.00, Validators.required],
+      paymentHealthInsurance: [0.00, Validators.required],
+      paymentDentalInsurance: [0.00, Validators.required],
+      paymentPetInsurance: [0.00, Validators.required],
+      paymentCarInsurance: [0.00, Validators.required],
 
-      paymentBankFees: ['', Validators.required],
-      paymentLoan: ['', Validators.required],
-      paymentCreditCard: ['', Validators.required],
-      paymentHirePurchases: ['', Validators.required],
-      paymentInvestments: ['', Validators.required],
-      paymentPension: ['', Validators.required],
+      paymentBankFees: [0.00, Validators.required],
+      paymentLoan: [0.00, Validators.required],
+      paymentCreditCard: [0.00, Validators.required],
+      paymentHirePurchases: [0.00, Validators.required],
+      paymentInvestments: [0.00, Validators.required],
+      paymentPension: [0.00, Validators.required],
 
-      paymentCarFuel: ['', Validators.required],
-      paymentCarTax: ['', Validators.required],
-      paymentCarMaintenance: ['', Validators.required],
-      paymentPublicTransport: ['', Validators.required],
-      paymentGym: ['', Validators.required],
-      paymentStreamingServices: ['', Validators.required],
-      paymentHolidays: ['', Validators.required],
+      paymentCarFuel: [0.00, Validators.required],
+      paymentCarTax: [0.00, Validators.required],
+      paymentCarMaintenance: [0.00, Validators.required],
+      paymentPublicTransport: [0.00, Validators.required],
+      paymentGym: [0.00, Validators.required],
+      paymentStreamingServices: [0.00, Validators.required],
+      paymentHolidays: [0.00, Validators.required],
+
+      paymentOther: [0.00, Validators.required],
     });
    }
 
@@ -92,8 +94,19 @@ export class BudgetPlannerComponent {
       // Process the form data here
       console.log(this.myForm.value);
       const formData = this.myForm.value;
-      const chartData = Object.values(formData).map(Number);
-      const chartLabels = Object.keys(formData);
+      const chartData = this.getChartData(Object.values(formData).map(Number)) ;
+      const chartLabels = [
+        "Income", 
+        "Household Bills", 
+        "Household Utilities", 
+        "Living Costs", 
+        "Friends and Family", 
+        "Pets", 
+        "Insurance", 
+        "Banking and Investments",
+        "Travel and Leisure", 
+        "Other",
+      ];
       // Create or update the doughnut chart
       this.updateDoughnutChart(chartLabels, chartData);
     }
@@ -127,5 +140,30 @@ export class BudgetPlannerComponent {
         }
       });
     }
+  }
+
+  getChartData(cd: number[]){
+    let chartData:number[] = [];
+    // add income to chartData
+    chartData.push(cd.slice(0,4).reduce((accumulator, currentValue) => accumulator + currentValue, 0));
+    // add HouseholdBills to chartData
+    chartData.push(cd.slice(4,7).reduce((accumulator, currentValue) => accumulator + currentValue, 0));
+    // add Household utilities to chartData
+    chartData.push(cd.slice(7, 16).reduce((accumulator, currentValue) => accumulator + currentValue, 0));
+    // add living costs to chartData
+    chartData.push(cd.slice(16,26).reduce((accumulator, currentValue) => accumulator + currentValue, 0));
+    // add friends and family to chartData
+    chartData.push(cd.slice(26, 30).reduce((accumulator, currentValue) => accumulator + currentValue, 0));
+    // add pets to chartData
+    chartData.push(cd.slice(30, 32).reduce((accumulator, currentValue) => accumulator + currentValue, 0));
+    // add insurance to chartData
+    chartData.push(cd.slice(32, 37).reduce((accumulator, currentValue) => accumulator + currentValue, 0));
+    // add banking investments to chartData
+    chartData.push(cd.slice(37, 43).reduce((accumulator, currentValue) => accumulator + currentValue, 0));
+    // add travel and leisure to chartData
+    chartData.push(cd.slice(43, 50).reduce((accumulator, currentValue) => accumulator + currentValue, 0));
+    // add other to chartData
+    chartData.push(cd.slice(50).reduce((accumulator, currentValue) => accumulator + currentValue, 0));
+    return chartData;
   }
 }

07d45ba4f4c5fddde95b84c66c69594dd8d2d831
Rachel Ring <rachelring84@gmail.com>
2024-03-27 11:30:31 +0000

    Added registration to the navbar

--- client-side/budget-app/src/app/navbar/navbar.component.ts
+++ client-side/budget-app/src/app/navbar/navbar.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterLink, MatToolbarModule],
+  imports: [CommonModule, RouterLink, MatToolbarModule, MatIconModule, MatMenuModule,],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })

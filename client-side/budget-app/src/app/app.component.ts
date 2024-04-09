69da663401f1e6f6eeb22392f9149cd6694ff4c6
Rachel Ring <rachelring84@gmail.com>
2024-04-09 15:16:03 +0100

    additional checks to make sure local storage has an access token after user refresh

--- client-side/budget-app/src/app/app.component.ts
+++ client-side/budget-app/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { AuthenticationService } from './shared/services/authentication.service';
 
 
 @Component({
@@ -13,5 +14,21 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  auth:boolean = false;
   title = 'budget-app';
+
+  constructor(private authService: AuthenticationService, @Inject(DOCUMENT) private document: Document){
+    const localStorage = document.defaultView?.localStorage;
+    if (localStorage){
+      this.auth = true;
+    }
+  }
+  
+  ngOnInit(): void {
+    if(this.auth){
+      if(this.authService.isUserAuthenticated()){
+        this.authService.sendAuthStateChangeNotification(true);
+      }
+    }
+  }
 }

732e85c1c5345aef6f83a7ce81e3a5a56e191221
Rachel Ring <rachelring84@gmail.com>
2024-04-12 15:50:25 +0100

    Generated new component user-budget to display a users budget

---
+++ client-side/budget-app/src/app/user/user-budget/user-budget.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user-budget',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-budget.component.html',
+  styleUrl: './user-budget.component.css'
+})
+export class UserBudgetComponent {
+
+}

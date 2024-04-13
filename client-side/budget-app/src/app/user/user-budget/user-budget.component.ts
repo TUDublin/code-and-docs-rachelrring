c886549e8a5d01cfeb28e60f7b8bc7ea540b5d67
Rachel Ring <rachelring84@gmail.com>
2024-04-13 11:38:47 +0100

    A user can view thier budget at /budget, the budget is displayed in tables - one table for income the other for expenses

--- client-side/budget-app/src/app/user/user-budget/user-budget.component.ts
+++ client-side/budget-app/src/app/user/user-budget/user-budget.component.ts
@@ -1,9 +1,10 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { CommonModule, DOCUMENT } from '@angular/common';
 import { AuthenticationService } from '../../shared/services/authentication.service';
-import { Router, ActivatedRoute, RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { UserBudgetResponseDto } from '../../_interfaces/response/UserBudgetResponseDto.model';
 import { HttpErrorResponse } from '@angular/common/http';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-user-budget',
@@ -11,6 +12,7 @@ import { HttpErrorResponse } from '@angular/common/http';
   imports: [
     CommonModule,
     RouterModule,
+    MatTableModule,
   ],
   templateUrl: './user-budget.component.html',
   styleUrl: './user-budget.component.css'
@@ -20,6 +22,9 @@ export class UserBudgetComponent implements OnInit {
   public auth: boolean = false;
   public hasBudget: boolean = false;
   public budget!: UserBudgetResponseDto;
+  public expensesDataSource!: MatTableDataSource<any>;
+  public incomeDataSource!: MatTableDataSource<any>;
+  displayedColumns = ['attribute', 'value'];
 
   constructor(
     private authService: AuthenticationService,
@@ -44,6 +49,17 @@ export class UserBudgetComponent implements OnInit {
             console.log(res);
             this.hasBudget = true;
             this.budget = res;
+            // this.dataSource = new MatTableDataSource<any>(Object.entries(this.budget).map(([key, value]) => ({ key: key, value: value })));
+            // Filter the key-value pairs to include only those where the key starts with "payment"
+            const paymentEntries = Object.entries(this.budget).filter(([key, _]) => key.startsWith('payment'));
+            const incomeEntries = Object.entries(this.budget).filter(([key, _]) => key.startsWith('income'));
+            // Map the filtered key-value pairs to an array of objects with 'key' and 'value' properties
+            this.expensesDataSource = new MatTableDataSource<any>(
+              paymentEntries.map(([key, value]) => ({ key: key, value: value }))
+            );
+            this.incomeDataSource = new MatTableDataSource<any>(
+              incomeEntries.map(([key, value]) => ({ key: key, value: value }))
+            );
           },
           error: (err: HttpErrorResponse) => {
             console.log(err);

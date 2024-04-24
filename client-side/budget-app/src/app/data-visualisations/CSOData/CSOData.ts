23c689ca64d6744943c64588f7ef93446bba1335
Rachel Ring <rachelring84@gmail.com>
2024-04-24 13:57:14 +0100

    issue #43 - two news graphs to show average monthly income by region

--- client-side/budget-app/src/app/data-visualisations/CSOData/CSOData.ts
+++ client-side/budget-app/src/app/data-visualisations/CSOData/CSOData.ts
@@ -5,3 +5,22 @@ export interface AvgEarningsResponseDTO{
 	ChildBenefit: number; 
 	InvestmentIncome: number; 
 }
+
+
+export interface HS067rowRegion {
+	IncomeType: string;
+	Value: number;
+  }
+  
+  export interface HS0672015Region {
+	State: HS067rowRegion[];
+	Border: HS067rowRegion[];
+	Midland: HS067rowRegion[];
+	West: HS067rowRegion[];
+	Dublin: HS067rowRegion[];
+	MidEast: HS067rowRegion[];
+	MidWest: HS067rowRegion[];
+	SouthEast: HS067rowRegion[];
+	SouthWest: HS067rowRegion[];
+  }
+  
--- client-side/budget-app/src/app/data-visualisations/data-visualisations.component.ts
+++ client-side/budget-app/src/app/data-visualisations/data-visualisations.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule, HttpErrorResponse } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { AvgEarningsResponseDTO } from './CSOData/CSOData';
+import { AvgEarningsResponseDTO, HS0672015Region } from './CSOData/CSOData';
 import { Chart } from 'chart.js/auto';
 
 
@@ -17,18 +17,36 @@ import { Chart } from 'chart.js/auto';
   styleUrl: './data-visualisations.component.css'
 })
 export class DataVisualisationsComponent implements OnInit {
-  public data!: AvgEarningsResponseDTO;
+  public dataAvgEarnings!: AvgEarningsResponseDTO;
+  public datahs067!: HS0672015Region;
+
   chart: any;
+  wagesChart: any;
+
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get<AvgEarningsResponseDTO>(environment.goUrlAddress).subscribe(
+    this.http.get<AvgEarningsResponseDTO>(environment.goUrlAddress + '/averageEarnings').subscribe(
       {
         next: (res: AvgEarningsResponseDTO) => {
-          console.log(res);
-          this.data = res;
+          this.dataAvgEarnings = res;
           this.createChart();
+        },
+        error: (err: HttpErrorResponse) => {
+          console.log(err);
+        }
+      }
+    );
+
+    this.http.get<HS0672015Region>(environment.goUrlAddress + '/hs067Region').subscribe(
+      {
+        next: (res: HS0672015Region) => {
+          console.log(res);
+          this.datahs067 = res;
+          this.createChartHS067(this.datahs067);
+          this.createWagesChart(this.datahs067);
+
         },
         error: (err: HttpErrorResponse) => {
           console.log(err);
@@ -44,8 +62,14 @@ export class DataVisualisationsComponent implements OnInit {
         labels: ["Employees Wages", "Self Employed", "Retirement Pension", "Child Benefit", "Investment Income"],
         datasets: [
           {
-            label: 'Income €',
-            data: [this.data.EmployeesWages, this.data.SelfEmployeed, this.data.RetirementPension, this.data.ChildBenefit, this.data.InvestmentIncome]
+            label: 'Income',
+            data: [
+              this.dataAvgEarnings.EmployeesWages,
+              this.dataAvgEarnings.SelfEmployeed,
+              this.dataAvgEarnings.RetirementPension,
+              this.dataAvgEarnings.ChildBenefit,
+              this.dataAvgEarnings.InvestmentIncome,
+            ]
           }
         ]
       },
@@ -59,9 +83,152 @@ export class DataVisualisationsComponent implements OnInit {
           title: {
             display: true,
             text: 'Average Household Income'
+          },
+          tooltip: {
+            callbacks: {
+              label: function (context) {
+                let label = context.dataset.label || '';
+
+                if (label) {
+                  label += ': ';
+                }
+                if (context.parsed.y !== null) {
+                  label += '€' + context.parsed.y.toFixed(2);
+                }
+                return label;
+              }
+            }
           }
         },
       }
     });
   }
+
+  createChartHS067(data: HS0672015Region) {
+    // Assuming you have a predefined list of income types that matches your data
+    const excludedIncomeType = "Employees-wages/salaries";
+    const incomeTypes = ["Self-employed income", "Retirement pensions", "Investment income", "Property income", "Child benefit", "Other direct income"]; // Define all other types  
+
+
+    const datasets = Object.keys(data).map(regionKey => {
+      const regionData = data[regionKey as keyof HS0672015Region];
+      const dataValues = incomeTypes.map(incomeType => {
+        const incomeData = regionData.find(row => row.IncomeType === incomeType);
+        return incomeData ? incomeData.Value : 0; // If not found, use 0
+      });
+
+      return {
+        label: regionKey,
+        data: dataValues,
+      };
+    });
+
+    this.chart = new Chart('barChartHS067', {
+      type: 'bar',
+      data: {
+        labels: incomeTypes,
+        datasets: datasets,
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          x: {
+            title: {
+              display: false,
+            },
+          },
+          y: {
+            beginAtZero: true,
+            title: {
+              display: true, // If you want to show the y-axis title
+              text: 'Amount (€)'
+            }
+          }
+        },
+        plugins: {
+          legend: {
+            position: 'right',
+          },
+          title: {
+            display: true,
+            text: 'Average Monthly Household Income by Region and Income Type',
+          },
+          tooltip: {
+            callbacks: {
+              label: function (context) {
+                let label = context.dataset.label || '';
+
+                if (label) {
+                  label += ': ';
+                }
+                if (context.parsed.y !== null) {
+                  label += '€' + context.parsed.y.toFixed(2);
+                }
+                return label;
+              }
+            }
+          },
+          
+        },
+      },
+    });
+
+    this.chart.render();
+  }
+
+  createWagesChart(data: HS0672015Region) {
+    const incomeType = "Employees-wages/salaries"; // Specify the exact income type label
+
+    const datasets = Object.keys(data).map(regionKey => {
+      const regionData = data[regionKey as keyof HS0672015Region];
+      const incomeData = regionData.find(row => row.IncomeType === incomeType);
+      const value = incomeData ? incomeData.Value : 0; // If not found, use 0 as value
+
+      return {
+        label: regionKey,
+        data: [value],
+      };
+    });
+
+    this.wagesChart = new Chart('wagesChart', {
+      type: 'bar',
+      data: {
+        labels: [incomeType],
+        datasets: datasets,
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            position: 'right',
+          },
+          title: {
+            display: true,
+            text: "Average Employee Wages by Region",
+          },
+          tooltip: {
+            callbacks: {
+              label: function (context) {
+                let label = context.dataset.label || '';
+
+                if (label) {
+                  label += ': ';
+                }
+                if (context.parsed.y !== null) {
+                  label += '€' + context.parsed.y.toFixed(2);
+                }
+                return label;
+              }
+            }
+          }
+        },
+      }
+    });
+
+    this.wagesChart.render();
+  }
+
+
 }

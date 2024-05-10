2d9e5b8d017525be1beb90db2b41f457205e0772
Rachel Ring <rachelring84@gmail.com>
2024-05-10 11:11:40 +0100

    #45 - Refactor tests to be more modular. Use of setup and etardown methods

--- server-side/main_test.go
+++ server-side/main_test.go
@@ -1,6 +1,51 @@
+// package main
+
+// import (
+// 	"net/http"
+// 	"net/http/httptest"
+// 	"testing"
+
+// 	"github.com/gin-gonic/gin"
+// 	"github.com/stretchr/testify/assert"
+// )
+
+// func TestGetHS067(t *testing.T) {
+// 	gin.SetMode(gin.TestMode)
+
+// 	r := gin.Default()
+// 	r.GET("/hs067", getHS067)
+
+// 	w := httptest.NewRecorder()
+// 	req, _ := http.NewRequest("GET", "/hs067", nil)
+// 	r.ServeHTTP(w, req)
+
+// 	assert.Equal(t, http.StatusOK, w.Code)
+// 	assert.Contains(t, w.Body.String(), "Employees-wages/salaries")
+// 	assert.Contains(t, w.Body.String(), "Self-employed income")
+// 	assert.Contains(t, w.Body.String(), "Retirement pensions")
+// 	assert.NotContains(t, w.Body.String(), "C02196V02652")
+// }
+
+// func TestGetHS067Region(t *testing.T) {
+// 	gin.SetMode(gin.TestMode)
+
+// 	r := gin.Default()
+// 	r.GET("/hs067Region", getHS067Region)
+
+// 	w := httptest.NewRecorder()
+// 	req, _ := http.NewRequest("GET", "/hs067", nil)
+// 	r.ServeHTTP(w, req)
+
+// 	assert.Equal(t, http.StatusOK, w.Code)
+// 	assert.Contains(t, w.Body.String(), "Employees-wages/salaries")
+// 	assert.Contains(t, w.Body.String(), "Self-employed income")
+// 	assert.NotContains(t, w.Body.String(), "C02196V02652")
+// }
+
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -9,16 +54,56 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetHS067(t *testing.T) {
-	gin.SetMode(gin.TestMode)
+// Setup and Teardown
+type TestSuite struct {
+	router *gin.Engine
+}
 
-	r := gin.Default()
-	r.GET("/hs067", getHS067)
+func (suite *TestSuite) SetupTest() {
+	gin.SetMode(gin.TestMode)
+	suite.router = gin.Default()
+	suite.router.GET("/hs067", getHS067)
+	suite.router.GET("/hs067Region", getHS067Region)
+}
+
+func (suite *TestSuite) TearDownTest() {
+	fmt.Print("teardown")
+}
+
+func TestMain(m *testing.M) {
+	// Set up here if needed
+	m.Run()
+}
+
+func TestHS067(t *testing.T) {
+	suite := &TestSuite{}
+	suite.SetupTest()
 
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/hs067", nil)
-	r.ServeHTTP(w, req)
+	suite.router.ServeHTTP(w, req)
 
 	assert.Equal(t, http.StatusOK, w.Code)
 	assert.Contains(t, w.Body.String(), "Employees-wages/salaries")
+	assert.Contains(t, w.Body.String(), "Self-employed income")
+	assert.Contains(t, w.Body.String(), "Retirement pensions")
+	assert.NotContains(t, w.Body.String(), "C02196V02652")
+
+	suite.TearDownTest()
+}
+
+func TestHS067Region(t *testing.T) {
+	suite := &TestSuite{}
+	suite.SetupTest()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/hs067Region", nil)
+	suite.router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Contains(t, w.Body.String(), "Employees-wages/salaries")
+	assert.Contains(t, w.Body.String(), "Self-employed income")
+	assert.NotContains(t, w.Body.String(), "C02196V02652")
+
+	suite.TearDownTest()
 }

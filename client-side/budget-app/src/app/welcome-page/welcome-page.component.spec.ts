ca4312f283e9ee9734b926df8e8d9a6eeabf4478
Rachel Ring <rachelring84@gmail.com>
2024-05-16 23:16:16 +0100

    #55 - additional testing

--- client-side/budget-app/src/app/welcome-page/welcome-page.component.spec.ts
+++ client-side/budget-app/src/app/welcome-page/welcome-page.component.spec.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { WelcomePageComponent } from './welcome-page.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
 
 describe('WelcomePageComponent', () => {
   let component: WelcomePageComponent;
@@ -27,4 +28,16 @@ describe('WelcomePageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have correct router links for navigation', () => {
+    const debugElements = fixture.debugElement.queryAll(By.directive(RouterLink));
+    const indexVisualisations = debugElements.findIndex(de => de.attributes['routerLink'] === "/visualisations");
+    const indexBudgetPlanner = debugElements.findIndex(de => de.attributes['routerLink'] === "/budget-planner");
+    const indexBudget = debugElements.findIndex(de => de.attributes['routerLink'] === "/budget");
+
+    expect(indexVisualisations).toBeGreaterThan(-1);
+    expect(indexBudgetPlanner).toBeGreaterThan(-1);
+    expect(indexBudget).toBeGreaterThan(-1);
+  });
+
 });
